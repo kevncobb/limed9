3c32c5789ea65b2ecfbf429c775e55c3af885f64
kevncobb <kevncobb@gmail.com>
2021-03-24 09:31:12 -0400

    added proper subtheme of varbase

---
+++ docroot/themes/custom/limesteone_doc/js/scripts.js
@@ -0,0 +1,12 @@
+/**
+ * @file
+ * Behaviors for the limestone_doc theme.
+ */
+
+(function($, _, Drupal) {
+  Drupal.behaviors.limestone_doc = {
+    attach: function() {
+      // limestone_doc JavaScript behaviors goes here.
+    }
+  };
+})(window.jQuery, window._, window.Drupal);
