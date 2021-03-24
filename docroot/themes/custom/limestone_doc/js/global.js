de52920fb8568aba54e520fee912ae5d28896fe2
kevncobb <kevncobb@gmail.com>
2021-03-24 09:55:36 -0400

    readded old theme

---
+++ docroot/themes/custom/limestone_doc/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.limestone_doc = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
