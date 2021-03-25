5e343ef8253496e9e6dc8fa2b00aaa82a287f95b
kevncobb <kevncobb@gmail.com>
2021-03-25 09:23:13 -0400

    updated search modal to focus in search bar after clicking

--- docroot/themes/custom/lime_doc/js/scripts.js
+++ docroot/themes/custom/lime_doc/js/scripts.js
@@ -9,4 +9,16 @@
       // lime_doc JavaScript behaviors goes here.
     }
   };
+  Drupal.behaviors.focus_search_bar_when_opened = {
+    attach: function (context, settings) {
+      $(context).find('button.btn.search-button').on('click touchstart', function () {
+        //event.stopPropagation();
+        //event.preventDefault();
+        var focus = function(){
+          $('form#search-block-form input.form-search').focus();
+        };
+        setTimeout(focus, 500);
+      });
+    }
+  };
 })(window.jQuery, window._, window.Drupal);
