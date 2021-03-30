4a678ad100ae0790ea36221a7a89632c0f0f4042
kevncobb <kevncobb@gmail.com>
2021-03-30 16:53:20 -0400

    added function for expanding main content

--- docroot/themes/custom/lime_doc/js/scripts.js
+++ docroot/themes/custom/lime_doc/js/scripts.js
@@ -12,12 +12,17 @@
   Drupal.behaviors.focus_search_bar_when_opened = {
     attach: function (context, settings) {
       $(context).find('button.btn.search-button').on('click touchstart', function () {
-        //event.stopPropagation();
-        //event.preventDefault();
         var focus = function(){
           $('form#search-block-form input.form-search').focus();
         };
         setTimeout(focus, 500);
+      });
+    }
+  };
+  Drupal.behaviors.expand_content_full_width = {
+    attach: function (context, settings) {
+      $(context).find('button.btn.expand-main-content').on('click touchstart', function () {
+        $('.node--type-page.node--view-mode-full').find('.col-sm-8.bs-region.bs-region--left').removeClass('col-sm-8');
       });
     }
   };
